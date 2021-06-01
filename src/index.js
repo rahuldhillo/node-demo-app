4467e382f6a5554bae52d053ed9b0da45b72b720
rahuldhillo <49671524+rahuldhillo@users.noreply.github.com>
2021-06-01 19:04:53 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.send("New hello 1234 world");
+});
+
+app.post("/post-anything", (req, res, next) => {
+  const { name, role } = req.body;
+  console.log({ name, role });
+  res.status(200).json(`The entered data is ${name} and ${role}`);
+});
+
+const DB_CONNECTION_STRING = `mongodb+srv://demo-node-crud:59CkPievhIc1HmsG@cluster0.ranyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => app.listen(8080, console.log(`Server running...`)))
+  .catch((err) => console.log(err));
