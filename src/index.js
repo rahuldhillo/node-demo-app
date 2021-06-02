f09ca00851c5369f1ba44eeb74d22af9366d09be
rahuldhillo <49671524+rahuldhillo@users.noreply.github.com>
2021-06-01 23:40:21 -0700

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
+  next();
   res.send("New hello 1234 world");
 });
 
 app.post("/post-anything", (req, res, next) => {
   const { name, role } = req.body;
   console.log({ name, role });
-  res.status(200).json(`The entered data is ${name} and ${role}`);
+  res.send(`The entered data is ${name} and ${role}`);
 });
 
 const DB_CONNECTION_STRING = `mongodb+srv://demo-node-crud:59CkPievhIc1HmsG@cluster0.ranyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
